5945d096e541efc0ba96539d8582573c8cb6a310
Matthew Hasteley <matthasteley@gmail.com>
2015-09-06 19:10:12 +0100

    Results respond to changes in input field +
    + More event listeners for keyboard use
    + Modified results so only uses inputted Windows Server if required

--- path_changer.js
+++ path_changer.js
@@ -1,60 +1,71 @@
-function itWentBadly(what, resultOne, resultTwo) {
+var itWentBadly = function () {
 	$("div.bad").addClass("overlay").addClass("active");
-	$(resultOne).html("&nbsp;");
-	$(resultTwo).html("&nbsp;");
 	$("div.result").removeClass("active");
 };
 
-function checkerAll() {
+var checkerAll = function () {
 	var alpha = $("#allIn").val().toLowerCase();
 	var beta = $("#windowsServer").val().toLowerCase();
 	var winMatch = alpha.indexOf(alpha.match(/\\\\[A-Za-z0-9]*\\[A-Za-z0-9]/));
 	var linMatch = alpha.indexOf(alpha.match(/[A-Za-z0-9]*\:\/\/[A-Za-z0-9]/));
 	var osxMatch = alpha.indexOf(alpha.match(/\/volumes\/[A-Za-z0-9]*/));
-	var pathStart = ["\\\\", "\\", "smb\:\/\/", "\/volumes\/"];
-	var test = function test(platform) {
-		var gamma = $("#allIn").val().toLowerCase();
-		var results = ["#allResultWin", "#allResultLin", "#allResultOSX"];
-		var resWin = gamma.replace(platform, pathStart[0].concat(beta).concat(pathStart[1])).replace(/\//g, "\\").replace(/\%20/g, " ");
-		var resLin = gamma.replace(platform, pathStart[2].concat(beta).concat(pathStart[1])).replace(/\\/g, "\/").replace(/\%20/g, " ");
-		var resOSX = gamma.replace(platform, pathStart[3]).replace(/\\/g, "\/").replace(/\%20/g, " ");
+	var pathStart = ["\\\\", "\\", "smb\:\/\/", "\/volumes\/", "/\\\\[A-Za-z0-9]*\\/", "/[A-Za-z0-9]*\:\/\/[A-Za-z0-9]*\//"];
+	var results = ["#allResultWin", "#allResultLin", "#allResultOSX"];
+	var osxReplace = function (platform) {
+		var resWin = alpha.replace(platform, pathStart[0].concat(beta).concat(pathStart[1])).replace(/\//g, "\\").replace(/\%20/g, " ");
+		var resLin = alpha.replace(platform, pathStart[2].concat(beta).concat(pathStart[1])).replace(/\\/g, "\/").replace(/\%20/g, " ");
+		var resOSX = alpha.replace(platform, pathStart[3]).replace(/\\/g, "\/").replace(/\%20/g, " ");
 		$(results[0]).html(resWin);
 		$(results[1]).html(resLin);
 		$(results[2]).html(resOSX);
-		$(this).next(".bad").html("");
+		$("div.result").addClass("active");
+		$("div.bad.overlay.active").removeClass("overlay").removeClass("active");
+	};
+	var otherReplace = function (platform, platformShort) {
+		var resWin = alpha.replace(platformShort, pathStart[0]).replace(/\//g, "\\").replace(/\%20/g, " ");
+		var resLin = alpha.replace(platformShort, pathStart[2]).replace(/\\/g, "\/").replace(/\%20/g, " ");
+		var resOSX = alpha.replace(platform, pathStart[3]).replace(/\\/g, "\/").replace(/\%20/g, " ");
+		$(results[0]).html(resWin);
+		$(results[1]).html(resLin);
+		$(results[2]).html(resOSX);
 		$("div.result").addClass("active");
 		$("div.bad.overlay.active").removeClass("overlay").removeClass("active");
 	}
 	if (winMatch == 0) {
-		test(/\\\\[A-Za-z0-9]*\\/);
+		otherReplace(/\\\\[A-Za-z0-9]*\\/, /\\\\/);
 	}
 	else if (linMatch == 0) {
-		test(/[A-Za-z0-9]*\:\/\/[A-Za-z0-9]*\//);
+		otherReplace(/[A-Za-z0-9]*\:\/\/[A-Za-z0-9]*\//, /[A-Za-z0-9]*\:\/\//);
 	}
 	else if (osxMatch == 0) {
-		test(/\/volumes\//);
+		osxReplace(/\/volumes\//);
 	}
 	else {
-		itWentBadly(this, "#osxResultLin", "#osxResultWin");
+		itWentBadly();
 	}
 };
 
-$("#allIn").keypress(
-	function enterKey(e) {
+var keyPresses = function enterKey(e) {
 		if (e.which == 13) {
 			$("#allBut").click();
 		}
-	});
+		else {
+			var alpha = $("#allIn").val().toLowerCase();
+			var osxMatch = alpha.indexOf(alpha.match(/\/volumes\/[A-Za-z0-9]*/));
+			if (osxMatch == 0) {
+				$("div.server").removeClass("hidden");
+			}
+			else {
+				$("div.server").addClass("hidden");
+				if ($("p#allResultWin").html().length > 0) {
+					$("div.result").removeClass("active");
+				}
+			}
+		}
+	};
+
 $("#allBut").click(checkerAll);
 
-$("#allIn").keyup(
-	function serverTest() {
-		var alpha = $("#allIn").val().toLowerCase();
-		var osxMatch = alpha.indexOf(alpha.match(/\/volumes\/[A-Za-z0-9]*/));
-		if (osxMatch == 0) {
-			$("div.server").removeClass("hidden");
-		}
-		else {
-			$("div.server").addClass("hidden");
-		}
-	});
+$("#allIn").keyup(keyPresses);
+
+$("#windowsServer").keyup(keyPresses);
