b9385743674c7025388f8c6d34ce2d560881fe68
Matthew Hasteley <matthastele@gmail.com>
2015-09-10 14:45:06 +0100

    Switched to all regex group replacements

--- path_changer.js
+++ path_changer.js
@@ -4,27 +4,26 @@ var itWentBadly = function () {
 };
 
 var checkerAll = function () {
-	var alpha = $("#allIn").val().toLowerCase();
+	var alpha = $("#allIn").val();
 	var beta = $("#windowsServer").val().toLowerCase();
-	var winMatch = alpha.indexOf(alpha.match(/\\\\[A-Za-z0-9]*\\[A-Za-z0-9]/));
-	var linMatch = alpha.indexOf(alpha.match(/[A-Za-z0-9]*\:\/\/[A-Za-z0-9]/));
-	var osxMatch = alpha.indexOf(alpha.match(/\/volumes\/[A-Za-z0-9]*/));
-	var pathStart = ["\\\\", "\\", "smb\:\/\/", "\/volumes\/"];
+	var winMatch = alpha.indexOf(alpha.match(/\\\\\S*/i));
+	var linMatch = alpha.indexOf(alpha.match(/\S+\:\/\/\S*/i));
+	var osxMatch = alpha.indexOf(alpha.match(/\/volumes\/\S*/i));
 	var results = ["#allResultWin", "#allResultLin", "#allResultOSX"];
 	var osxReplace = function (platform) {
-		var resWin = alpha.replace(platform, pathStart[0].concat(beta).concat(pathStart[1])).replace(/\//g, "\\").replace(/\%20/g, " ");
-		var resLin = alpha.replace(platform, pathStart[2].concat(beta).concat(pathStart[1])).replace(/\\/g, "\/").replace(/\%20/g, " ");
-		var resOSX = alpha.replace(platform, pathStart[3]).replace(/\\/g, "\/").replace(/\%20/g, " ");
+		var resWin = alpha.replace(platform, "\\\\" + beta + "\\" + "$2").replace(/\//g, "\\").replace(/\%20/g, " ");
+		var resLin = alpha.replace(platform, "smb:\/\/" + beta + "\/" + "$2").replace(/\\/g, "\/").replace(/\%20/g, " ");
+		var resOSX = alpha.replace(platform, "\/volumes\/$2").replace(/\\/g, "\/").replace(/\%20/g, " ");
 		$(results[0]).html(resWin);
 		$(results[1]).html(resLin);
 		$(results[2]).html(resOSX);
 		$("div.result").addClass("active");
 		$("div.bad.overlay.active").removeClass("overlay").removeClass("active");
 	};
-	var otherReplace = function (platform, platformShort) {
-		var resWin = alpha.replace(platformShort, pathStart[0]).replace(/\//g, "\\").replace(/\%20/g, " ");
-		var resLin = alpha.replace(platformShort, pathStart[2]).replace(/\\/g, "\/").replace(/\%20/g, " ");
-		var resOSX = alpha.replace(platform, pathStart[3]).replace(/\\/g, "\/").replace(/\%20/g, " ");
+	var otherReplace = function (platform) {
+		var resWin = alpha.replace(platform, "\\\\$2$3").replace(/\//g, "\\").replace(/\%20/g, " ");
+		var resLin = alpha.replace(platform, "smb:\/\/$2$3").replace(/\\/g, "\/").replace(/\%20/g, " ");
+		var resOSX = alpha.replace(platform, "\/volumes$3").replace(/\\/g, "\/").replace(/\%20/g, " ");
 		$(results[0]).html(resWin);
 		$(results[1]).html(resLin);
 		$(results[2]).html(resOSX);
@@ -32,13 +31,13 @@ var checkerAll = function () {
 		$("div.bad.overlay.active").removeClass("overlay").removeClass("active");
 	}
 	if (winMatch == 0) {
-		otherReplace(/\\\\[A-Za-z0-9]*\\/, /\\\\/);
+		otherReplace(/(\\\\)([^\\\/]*)(\S*)/i);
 	}
 	else if (linMatch == 0) {
-		otherReplace(/[A-Za-z0-9]*\:\/\/[A-Za-z0-9]*\//, /[A-Za-z0-9]*\:\/\//);
+		otherReplace(/(\S+\:\/\/)([^\\\/]*)(\S*)/i);
 	}
 	else if (osxMatch == 0) {
-		osxReplace(/\/volumes\//);
+		osxReplace(/(\/\S+\/)(\S*)/i);
 	}
 	else {
 		itWentBadly();
@@ -46,23 +45,23 @@ var checkerAll = function () {
 };
 
 var keyPresses = function enterKey(e) {
-		if (e.which == 13) {
-			$("#allBut").click();
+	if (e.which == 13) {
+		$("#allBut").click();
+	}
+	else {
+		var alpha = $("#allIn").val().toLowerCase();
+		var osxMatch = alpha.indexOf(alpha.match(/\/volumes\/\S*/i));
+		if (osxMatch == 0) {
+			$("div.server").removeClass("hidden");
 		}
 		else {
-			var alpha = $("#allIn").val().toLowerCase();
-			var osxMatch = alpha.indexOf(alpha.match(/\/volumes\/[A-Za-z0-9]*/));
-			if (osxMatch == 0) {
-				$("div.server").removeClass("hidden");
-			}
-			else {
-				$("div.server").addClass("hidden");
-				if ($("p#allResultWin").html().length > 0) {
-					$("div.result").removeClass("active");
-				}
+			$("div.server").addClass("hidden");
+			if ($("p#allResultWin").html().length > 0) {
+				$("div.result").removeClass("active");
 			}
 		}
-	};
+	}
+};
 
 $("#allBut").click(checkerAll);
 
